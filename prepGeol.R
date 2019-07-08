d2dd3ae0d38e24c69fd8b0185671922cf3bb7ca8
Raphaël Aussenac <raphael.aussenac@gmail.com>
2019-07-08 22:36:39 +0200

    remove pnr mask from geol data (useless)

--- prepGeol.R
+++ prepGeol.R
@@ -8,6 +8,7 @@ rm(list = ls())
 # load packages
 library(rgdal)
 library(raster)
+library(gdalUtils)
 
 # set work directory
 setwd("C:/Users/raphael.aussenac/Documents/GitHub/PROTEST")
@@ -16,18 +17,20 @@ setwd("C:/Users/raphael.aussenac/Documents/GitHub/PROTEST")
 savoie <- readOGR(dsn = "W:/BRGM/BD Charm-50/73", layer = "GEO050K_HARM_073_S_FGEOL_CGH_2154", encoding = "UTF-8", use_iconv = TRUE)
 hsavoie <- readOGR(dsn = "W:/BRGM/BD Charm-50/74", layer = "GEO050K_HARM_074_S_FGEOL_CGH_2154", encoding = "UTF-8", use_iconv = TRUE)
 
-# bind the two regions
-shs <- bind(savoie, hsavoie)
-plot(shs, col = shs$CODE, border = shs$CODE)
-
 # load a mask of the study area
 PNR <- readOGR(dsn = "Z:/Private/PNR Bauges/Sans_Trou", layer = "parc_filled", encoding = "UTF-8", use_iconv = TRUE)
+
+# bind the two regions
+shs <- bind(savoie, hsavoie)
+shs <- spTransform(shs, crs(PNR)) # change shs projection
+shs <- crop(shs, extent(925930, 968160, 6489455, 6538375)) # reduce extent
+plot(shs, col = shs$NOTATION, border = shs$NOTATION)
 plot(PNR, add = TRUE, col = "green")
 
-# intersect
-shs <- spTransform(shs, crs(PNR)) # change shs projection
-geol <- intersect(shs, PNR)
-plot(geol, col = geol$CODE, border = geol$CODE)
+# # intersect
+# geol <- intersect(shs, PNR)
+# plot(geol, col = geol$CODE, border = geol$CODE)
+# shapefile(geol, filename = 'geol')
 
 # save new shapefile
-shapefile(geol, filename = 'geol')
+shapefile(shs, filename = 'geol', overwrite = TRUE)
